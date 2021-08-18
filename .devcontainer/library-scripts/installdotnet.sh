9536e21984c99ffb858de59d1f1d7fffe3f2c66d
Brian Sheridan <brian.sheridan@gmail.com>
2021-08-18 14:57:19 +0000

    Updated dotnet install script

--- .devcontainer/library-scripts/installdotnet.sh
+++ .devcontainer/library-scripts/installdotnet.sh
@@ -2,6 +2,7 @@ mkdir $HOME/dotnet_install
 cd $HOME/dotnet_install
 wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
-rm -rf $HOME/dotnet_install
+cd ..
+rm -rf dotnet_install
 
 sudo apt-get update; sudo apt-get install -y apt-transport-https &&  sudo apt-get update &&  sudo apt-get install -y dotnet-sdk-3.1
