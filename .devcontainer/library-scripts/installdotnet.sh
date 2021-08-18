77a874ea329dc5d34af6aaea53aa044793dc9ed1
Brian Sheridan <brian.sheridan@gmail.com>
2021-08-18 14:50:59 +0000

    Updated to install .net 3.1

--- .devcontainer/library-scripts/installdotnet.sh
+++ .devcontainer/library-scripts/installdotnet.sh
@@ -1,4 +1,7 @@
 mkdir $HOME/dotnet_install
 cd $HOME/dotnet_install
-curl -L https://dot.net/v1/dotnet-install.sh  -o install-dotnet.sh
-bash install-dotnet.sh -c 3.1
+wget https://packages.microsoft.com/config/ubuntu/21.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm -rf $HOME/dotnet_install
+
+sudo apt-get update; sudo apt-get install -y apt-transport-https &&  sudo apt-get update &&  sudo apt-get install -y dotnet-sdk-3.1
