bbe3659d5d6625a6b7d1352d025082fd772bd5cb
Brian Sheridan <brian.sheridan@gmail.com>
2021-08-18 21:59:40 +0000

    TD Ameritrade Tests

---
+++ tests/tdameritrade/MappingTests.cs
@@ -0,0 +1,131 @@
+using System.Collections.Generic;
+using AutoFixture.Xunit2;
+using External.Ameritrade.API.Mappings;
+using External.Ameritrade.API.Models.Options;
+using Shouldly;
+using Xunit;
+
+namespace Tests.TDAmeritrade
+{
+    public class MappingTests
+    {
+        public OptionChain _optionChain;
+       
+        public MappingTests()
+        {
+            _optionChain = TestHelper.CreateOption();
+            
+            var put = TestHelper.CreateOption("PUT");
+
+            var puts = new Option[] { put };
+
+            var call = TestHelper.CreateOption("CALL");
+
+            var calls = new Option[] {call};
+
+            _optionChain.Puts.Add("2021-07-09:5",new Dictionary<string, Option[]>(){ {"138.0",puts}});
+            _optionChain.Calls.Add("2021-07-09:5",new Dictionary<string, Option[]>(){ {"138.0",calls}});
+            
+        }
+
+        private Option GetPutOption()
+        {
+            return _optionChain.Puts["2021-07-09:5"]["138.0"][0];
+        }
+
+        [Theory,AutoData]
+        public void CheckStock_NotNull(OptionChain optionChain)
+        {
+            var mapper = new OptionChainToStock(optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.ShouldNotBeNull();
+        }
+
+        [Theory,AutoData]
+        public void CheckStock_Map_Symbol(OptionChain optionChain)
+        {
+            var mapper = new OptionChainToStock(optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Symbol.ShouldBe(optionChain.Symbol);
+        }
+
+        [Theory,AutoData]
+        public void CheckStock_Map_Price(OptionChain optionChain)
+        {
+            var mapper = new OptionChainToStock(optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Price.ShouldBe(optionChain.UnderlyingPrice);
+        }
+
+        [Fact]
+        public void CheckStock_Map_OptionList()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options.Count.ShouldBeGreaterThan(0);
+        }
+
+        [Fact]
+        public void CheckStock_Map_ID()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options[0].Id.ShouldBe(GetPutOption().Symbol);
+        }
+        [Fact]
+        public void CheckStock_Map_PutCall()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options[0].PutCall.ToString().ShouldBe(GetPutOption().PutCall);
+        }
+         [Fact]
+        public void CheckStock_Map_Delta()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options[0].Delta.ShouldBe(GetPutOption().Delta);
+        }
+        [Fact]
+        public void CheckStock_Map_Vega()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options[0].Vega.ShouldBe(GetPutOption().Vega);
+        }
+        [Fact]
+        public void CheckStock_Map_MidPrice()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options[0].Mid.ShouldBe(GetPutOption().Mark);
+        }
+        [Fact]
+        public void CheckStock_Map_ImpliedVolatility()
+        {
+            var mapper = new OptionChainToStock(_optionChain);
+
+            var stock = mapper.MapToStock();
+
+            stock.Options[0].ImpliedVolatility.ShouldBe(GetPutOption().Volatility);
+        }
+    }
+}
---
+++ tests/tdameritrade/OptionChainTests.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using System.Text.Json;
+using External.Ameritrade.API.Models.Options;
+using Shouldly;
+using Xunit;
+
+namespace Tests.TDAmeritrade
+{
+    public class OptionChainTests
+    {
+        private readonly OptionChain _optionChain;
+        private readonly string _jsonString;
+        public OptionChainTests()
+        {
+            _jsonString = File.ReadAllText(@"files/optionchain.json");                        
+            _optionChain = JsonSerializer.Deserialize<OptionChain>(_jsonString);
+        }
+
+        [Fact]
+        public void FileHadData() => _jsonString.ShouldNotBeNullOrEmpty();
+
+        [Fact]
+        public void ValidMaps_Symbol() => _optionChain.Symbol.ShouldBe("AAPL");
+
+        [Fact]
+        public void ValidMaps_Option_Puts_ShouldNotBeNull()
+        {
+            var puts = _optionChain.Puts["2021-07-09:5"]["138.0"];
+            puts.ShouldNotBeEmpty();
+        }
+
+        [Fact]
+        public void ValidMaps_Option_Puts_DaysToExpire_ShouldBe5()
+        {
+            var puts = _optionChain.Puts["2021-07-09:5"]["138.0"];
+            puts[0].DaysToExpiration.ShouldBe(5);
+        }
+
+
+        [Fact]
+        public void ValidMaps_Option_Calls_ShouldNotBeNull()
+        {
+            var calls = _optionChain.Calls["2021-07-09:5"]["138.0"] ;
+            calls.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void ValidMaps_Option_Calls_DaysToExpire_ShouldBe5()
+        {
+            var calls = _optionChain.Calls["2021-07-09:5"]["138.0"] ;
+            calls[0].DaysToExpiration.ShouldBe(5);
+        }
+    }
+}
