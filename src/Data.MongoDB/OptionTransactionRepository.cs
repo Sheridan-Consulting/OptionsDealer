ba2e5eb02e22ba936a167ac5c661e86b662f1fca
Brian Sheridan <brian.sheridan@gmail.com>
2021-08-19 20:58:59 +0000

    Find by Id

--- src/Data.MongoDB/OptionTransactionRepository.cs
+++ src/Data.MongoDB/OptionTransactionRepository.cs
@@ -4,6 +4,10 @@ namespace Data.MongoDB
 {
     public class OptionTransactionRepository : IOptionTransactionRepository
     {
-        
+        public async Task<OptionTransactionBase> GetAsync(int optionTransactionId)
+        {
+            var filter = Builders<OptionTransactionBase>.Filter.Eq("Id", optionTransactionId);
+            return await _context.OptionTransactions.Find(filter).FirstOrDefaultAsync();                                    
+        }
     }
 }
